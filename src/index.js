dbb4f8b7687088a10c942a81d037c7b27b658138
aysenurdavulcu <75391618+aysenurdavulcu@users.noreply.github.com>
2021-10-31 11:35:14 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BoardSwitcher from './BoardSwitcher';
-import './index.css';
-
-ReactDOM.render(
-  <BoardSwitcher numBoards={3} />,
-  document.getElementById('root')
-);
